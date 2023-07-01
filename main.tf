c975a4f898f239c4cc6505edf984dcdcc7c1a0ed
nozomi-koborinai <kobofender@gmail.com>
2023-07-01 23:55:29 +0900

    fix: コメント

--- main.tf
+++ main.tf
@@ -1,64 +1,47 @@
-# Creates a new Google Cloud project .
+# Firebase プロジェクト用の Google Cloud プロジェクトを立ち上げる
 resource "google_project" "default" {
-  provider = google-beta.no_user_project_override
+  provider = google-beta
 
-  name       = "terraform-koborinai"
-  project_id = "terraform-koborinai"
-
-  # Required for any service that requires the Blaze pricing plan
-  # (like Firecbase Authentication with GCIP)
+  # project_id は全世界で一意になる必要がある
+  project_id = "firebase-terraform"
+  name       = "firebase-terraform"
   billing_account = var.billing_account
 
-  # Required for the project to display in any list of Firebase projects.
+  # Firebase のプロジェクトとして表示するために必要
   labels = {
     "firebase" = "enabled"
   }
 }
 
-# Enables required APIs .
-resource "google_project_service" "default" {
-  provider = google-beta.no_user_project_override
-  project  = google_project.default.project_id
-  for_each = toset([
-    "cloudbilling.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
-    "firebase.googleapis.com",
-    # Enabling the ServiceUsage API allows the new project to be quota checked from now on.
-    "serviceusage.googleapis.com",
-  ])
-  service = each.key
-  # Don't disable the service if the resource block is removed by accident.
-  disable_on_destroy = false
-}
-
-# Enables Firebase services for the new project created above.
+# Firebase のプロジェクトを立ち上げる
 resource "google_firebase_project" "default" {
   provider = google-beta
   project  = google_project.default.project_id
+
   depends_on = [
-    google_project_service.default
+    google_project_service.default,
   ]
 }
 
-resource "google_project_service" "firestore" {
-  project = google_project.default.project_id
-  service = "firestore.googleapis.com"
+# 各種APIを有効化する
+resource "google_project_service" "default" {
+  provider = google-beta
+  project  = google_project.default.project_id
+  for_each = toset([
+    "cloudbuild.googleapis.com",
+    "firestore.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "identitytoolkit.googleapis.com",
+  ])
+  service = each.key
+  disable_on_destroy = false
 }
 
-resource "google_firebase_project_location" "basic" {
+resource "google_firebase_project_location" "default" {
   provider = google-beta
   project  = google_firebase_project.default.project
 
   location_id = "asia-northeast1"
-}
-
-resource "google_firestore_database" "database" {
-  project                     = google_project.default.project_id
-  name                        = "(default)"
-  location_id                 = google_firebase_project_location.basic.location_id
-  type                        = "FIRESTORE_NATIVE"
-  concurrency_mode            = "OPTIMISTIC"
-  app_engine_integration_mode = "DISABLED"
-
-  depends_on = [google_project_service.firestore]
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,3 @@
-# Terraform configuration to set up providers by version .
 terraform {
   required_providers {
     google-beta = {
@@ -8,14 +7,13 @@ terraform {
   }
 }
 
-# Configures the provider to use the resource block's specified project for quota checks.
 provider "google-beta" {
   user_project_override = true
 }
 
-# Configures the provider to not use the resource block's specified project for quota checks.
-# This provider should only be used during project creation and initializing services.
-provider "google-beta" {
-  alias                 = "no_user_project_override"
-  user_project_override = false
-}
+# # Configures the provider to not use the resource block's specified project for quota checks.
+# # This provider should only be used during project creation and initializing services.
+# provider "google-beta" {
+#   alias                 = "no_user_project_override"
+#   user_project_override = false
+# }
