e27ae53ede29924de1f0848fe63696bb55b29d3c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-07-02 18:39:44 +0900

    fix: Firestore リソース依存関係整理

--- modules/firestore/main.tf
+++ modules/firestore/main.tf
@@ -17,16 +17,20 @@ resource "google_firestore_document" "mydoc" {
   collection  = "somenewcollection"
   document_id = "my-doc-id"
   fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"akey\":{\"stringValue\":\"avalue\"}}}}}"
+  depends_on = [google_firestore_database.default]
 }
 resource "google_firestore_document" "sub_document" {
   project     = var.project_id
   collection  = "${google_firestore_document.mydoc.path}/subdocs"
   document_id = "bitcoinkey"
   fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"ayo\":{\"stringValue\":\"val2\"}}}}}"
+  depends_on = [google_firestore_database.default]
+
 }
 resource "google_firestore_document" "sub_sub_document" {
   project     = var.project_id
   collection  = "${google_firestore_document.sub_document.path}/subsubdocs"
   document_id = "asecret"
   fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"secret\":{\"stringValue\":\"hithere\"}}}}}"
+  depends_on = [google_firestore_database.default]
 }
