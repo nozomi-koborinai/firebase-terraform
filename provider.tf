6f1127e02d7ba055315b1c185c264a5689655331
nozomi-koborinai <kobofender@gmail.com>
2023-07-06 17:20:14 +0900

    remove: comment

--- provider.tf
+++ provider.tf
@@ -12,8 +12,6 @@ provider "google-beta" {
   billing_project       = var.project_id
 }
 
-# Configures the provider to not use the resource block's specified project for quota checks.
-# This provider should only be used during project creation and initializing services.
 provider "google-beta" {
   alias                 = "no_user_project_override"
   user_project_override = false
