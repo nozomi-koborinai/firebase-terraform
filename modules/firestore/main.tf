f3ccfa393acb66e1126ccf6cfa31eb76f88a5570
nozomi-koborinai <kobofender@gmail.com>
2023-07-06 10:59:24 +0900

    style: terraform fmt

--- modules/firestore/main.tf
+++ modules/firestore/main.tf
@@ -1,10 +1,10 @@
 # Firebase Firestore インスタンス
 resource "google_firestore_database" "default" {
-  project          = var.project_id
-  name             = "(default)"
-  location_id      = var.location
-  type             = "FIRESTORE_NATIVE"
-  concurrency_mode = "OPTIMISTIC"
+  project                     = var.project_id
+  name                        = "(default)"
+  location_id                 = var.location
+  type                        = "FIRESTORE_NATIVE"
+  concurrency_mode            = "OPTIMISTIC"
   app_engine_integration_mode = "DISABLED"
 
   depends_on = [
